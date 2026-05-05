289cde4b7942a49e8b88e0bb9c90123b5128e521
David Masegosa <davidmasegosa@gmail.com>
2026-05-05 13:21:29 +0200

    feat: create Numbers model

---
+++ assets/js/models/numbers.js
@@ -0,0 +1,66 @@
+class Numbers {
+
+    constructor(ctx) {
+        this.ctx = ctx
+
+        this.number0Sprite = new Image()
+        this.number0Sprite.src = './assets/images/sprites/numbers/0.png'
+        this.number0Sprite.onload = () => {
+            this.number0Sprite.isReady = true
+        }
+
+        this.number1Sprite = new Image()
+        this.number1Sprite.src = './assets/images/sprites/numbers/1.png'
+        this.number1Sprite.onload = () => {
+            this.number1Sprite.isReady = true
+        }
+
+        this.number2Sprite = new Image()
+        this.number2Sprite.src = './assets/images/sprites/numbers/2.png'
+        this.number2Sprite.onload = () => {
+            this.number2Sprite.isReady = true
+        }
+
+        this.number3Sprite = new Image()
+        this.number3Sprite.src = './assets/images/sprites/numbers/3.png'
+        this.number3Sprite.onload = () => {
+            this.number3Sprite.isReady = true
+        }
+
+        this.number4Sprite = new Image()
+        this.number4Sprite.src = './assets/images/sprites/numbers/4.png'
+        this.number4Sprite.onload = () => {
+            this.number4Sprite.isReady = true
+        }
+
+        this.number5Sprite = new Image()
+        this.number5Sprite.src = './assets/images/sprites/numbers/5.png'
+        this.number5Sprite.onload = () => {
+            this.number5Sprite.isReady = true
+        }
+
+        this.number6Sprite = new Image()
+        this.number6Sprite.src = './assets/images/sprites/numbers/6.png'
+        this.number6Sprite.onload = () => {
+            this.number6Sprite.isReady = true
+        }
+
+        this.number7Sprite = new Image()
+        this.number7Sprite.src = './assets/images/sprites/numbers/7.png'
+        this.number7Sprite.onload = () => {
+            this.number7Sprite.isReady = true
+        }
+
+        this.number8Sprite = new Image()
+        this.number8Sprite.src = './assets/images/sprites/numbers/8.png'
+        this.number8Sprite.onload = () => {
+            this.number8Sprite.isReady = true
+        }
+
+        this.number9Sprite = new Image()
+        this.number9Sprite.src = './assets/images/sprites/numbers/9.png'
+        this.number9Sprite.onload = () => {
+            this.number9Sprite.isReady = true
+        }
+    }
+}
--- assets/js/models/panels/score-panel.js
+++ assets/js/models/panels/score-panel.js
@@ -9,65 +9,7 @@ class ScorePanel {
             this.textSprite.isReady = true
         }
 
-        this.number0Sprite = new Image()
-        this.number0Sprite.src = './assets/images/sprites/panels/score/0.png'
-        this.number0Sprite.onload = () => {
-            this.number0Sprite.isReady = true
-        }
-
-        this.number1Sprite = new Image()
-        this.number1Sprite.src = './assets/images/sprites/panels/score/1.png'
-        this.number1Sprite.onload = () => {
-            this.number1Sprite.isReady = true
-        }
-
-        this.number2Sprite = new Image()
-        this.number2Sprite.src = './assets/images/sprites/panels/score/2.png'
-        this.number2Sprite.onload = () => {
-            this.number2Sprite.isReady = true
-        }
-
-        this.number3Sprite = new Image()
-        this.number3Sprite.src = './assets/images/sprites/panels/score/3.png'
-        this.number3Sprite.onload = () => {
-            this.number3Sprite.isReady = true
-        }
-
-        this.number4Sprite = new Image()
-        this.number4Sprite.src = './assets/images/sprites/panels/score/4.png'
-        this.number4Sprite.onload = () => {
-            this.number4Sprite.isReady = true
-        }
-
-        this.number5Sprite = new Image()
-        this.number5Sprite.src = './assets/images/sprites/panels/score/5.png'
-        this.number5Sprite.onload = () => {
-            this.number5Sprite.isReady = true
-        }
-
-        this.number6Sprite = new Image()
-        this.number6Sprite.src = './assets/images/sprites/panels/score/6.png'
-        this.number6Sprite.onload = () => {
-            this.number6Sprite.isReady = true
-        }
-
-        this.number7Sprite = new Image()
-        this.number7Sprite.src = './assets/images/sprites/panels/score/7.png'
-        this.number7Sprite.onload = () => {
-            this.number7Sprite.isReady = true
-        }
-
-        this.number8Sprite = new Image()
-        this.number8Sprite.src = './assets/images/sprites/panels/score/8.png'
-        this.number8Sprite.onload = () => {
-            this.number8Sprite.isReady = true
-        }
-
-        this.number9Sprite = new Image()
-        this.number9Sprite.src = './assets/images/sprites/panels/score/9.png'
-        this.number9Sprite.onload = () => {
-            this.number9Sprite.isReady = true
-        }
+        this.numbers = new Numbers(ctx)
     }
 
     draw (score) {
@@ -80,16 +22,16 @@ class ScorePanel {
 
         if (
             this.textSprite.isReady &&
-            this.number0Sprite.isReady &&
-            this.number1Sprite.isReady &&
-            this.number2Sprite.isReady &&
-            this.number3Sprite.isReady &&
-            this.number4Sprite.isReady &&
-            this.number5Sprite.isReady &&
-            this.number6Sprite.isReady &&
-            this.number7Sprite.isReady &&
-            this.number8Sprite.isReady &&
-            this.number9Sprite.isReady
+            this.numbers.number0Sprite.isReady &&
+            this.numbers.number1Sprite.isReady &&
+            this.numbers.number2Sprite.isReady &&
+            this.numbers.number3Sprite.isReady &&
+            this.numbers.number4Sprite.isReady &&
+            this.numbers.number5Sprite.isReady &&
+            this.numbers.number6Sprite.isReady &&
+            this.numbers.number7Sprite.isReady &&
+            this.numbers.number8Sprite.isReady &&
+            this.numbers.number9Sprite.isReady
         )
         {
             this.ctx.drawImage(
@@ -103,7 +45,7 @@ class ScorePanel {
             const digits = panelScore.split('')
 
                 digits.forEach((digit, index) => {
-                    const sprite = this[`number${digit}Sprite`]
+                    const sprite = this.numbers[`number${digit}Sprite`]
 
                     this.ctx.drawImage(
                         sprite,
